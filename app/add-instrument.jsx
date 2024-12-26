198ddae3d8e7ee41d929ef7416cebfbcd3e48c82
Dmitry Kovalev <trigger1610@gmail.com>
2024-12-26 18:30:56 +0400

    Add Save button

--- app/add-instrument.jsx
+++ app/add-instrument.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react"
-import { StyleSheet, Text, TextInput, View } from "react-native"
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import DropDownPicker from "react-native-dropdown-picker"
 import { Colors } from "@/constants/Colors"
+import { LinearGradient } from "expo-linear-gradient"
+import { useRouter } from "expo-router"
 
 const TITLE_TEXT = "MY GUITARS"
+const SAVE_BUTTON_TEXT = "Save"
+const TYPE_PLACEHOLDER = "Guitar type"
+const NAME_PLACEHOLDER = "Guitar name"
+
+const data = Object.freeze([
+  { label: "Electro", value: "electro" },
+  { label: "Bass", value: "bass" },
+  { label: "Acoustic", value: "acoustic" },
+  { label: "Ukulele", value: "ukulele" },
+  { label: "Classic", value: "classic" },
+])
 
 export default function AddInstrument() {
-  const data = [
-    { label: "Electro", value: "electro" },
-    { label: "Bass", value: "bass" },
-    { label: "Acoustic", value: "acoustic" },
-    { label: "Ukulele", value: "ukulele" },
-    { label: "Classic", value: "classic" },
-  ]
+  const router = useRouter()
 
   const [open, setOpen] = useState(false)
   const [value, setValue] = useState(null)
@@ -26,35 +33,60 @@ export default function AddInstrument() {
       style={styles.instrument}
     >
       <Text style={styles.title}>{TITLE_TEXT}</Text>
-
       <View style={styles.inputs}>
         <TextInput
-          style={styles.input}
           placeholderTextColor={Colors.dark.placeholder}
-          placeholder="Guitar name"
+          placeholder={NAME_PLACEHOLDER}
+          style={styles.input}
         ></TextInput>
         <DropDownPicker
-          style={styles.input}
           items={data}
           open={open}
-          setOpen={setOpen}
           value={value}
+          setOpen={setOpen}
           setValue={setValue}
-          placeholder="Guitar type"
-          placeholderStyle={{ color: Colors.dark.placeholder }}
-          textStyle={{ color: Colors.dark.text, fontSize: 16 }}
-          dropDownContainerStyle={{
-            backgroundColor: Colors.dark.background,
-            borderColor: Colors.dark.accent,
-          }}
+          placeholder={TYPE_PLACEHOLDER}
+          placeholderStyle={styles.placeholder}
           disableBorderRadius={false}
+          style={styles.input}
+          textStyle={styles.textStyle}
+          dropDownContainerStyle={styles.dropdownStyle}
         />
       </View>
+
+      <Pressable style={styles.buttonContainer} onPress={() => router.back()}>
+        <LinearGradient
+          colors={["#D68424", "#6E3619"]}
+          style={styles.gradient}
+          end={{ x: 0.6, y: 1.5 }}
+          locations={[0.1, 1]}
+        >
+          <Text style={styles.buttonText}>{SAVE_BUTTON_TEXT}</Text>
+        </LinearGradient>
+      </Pressable>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  buttonContainer: { marginBottom: 60 },
+  buttonText: {
+    color: Colors.dark.text,
+    fontSize: 20,
+    textAlign: "center",
+  },
+  dropdownStyle: {
+    backgroundColor: Colors.dark.background,
+    borderColor: Colors.dark.accent,
+  },
+  textStyle: { color: Colors.dark.text, fontSize: 16 },
+  gradient: {
+    padding: 20,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   inputs: {
     marginTop: 75,
     flex: 1,
@@ -76,6 +108,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.dark.background,
     fontSize: 16,
   },
+  placeholder: { color: Colors.dark.placeholder },
   title: {
     fontSize: 20,
     fontWeight: "400",
