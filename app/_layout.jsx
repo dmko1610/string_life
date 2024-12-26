3d91f3ff81d01716dec876d81dc08518752970cd
Dmitry Kovalev <trigger1610@gmail.com>
2024-12-26 18:38:19 +0400

    Fix flickering

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,17 @@
 import React from "react"
 import { Stack } from "expo-router"
 import { StatusBar } from "expo-status-bar"
+import { Colors } from "@/constants/Colors"
+import { StyleSheet, View } from "react-native"
 
 export default function RootLayout() {
   return (
-    <>
+    <View style={styles.container}>
       <StatusBar style="auto" />
       <Stack
         screenOptions={{
-          headerStyle: { backgroundColor: "#151515" },
-          headerTintColor: "#fff",
+          headerStyle: { backgroundColor: Colors.dark.background },
+          headerTintColor: Colors.dark.text,
         }}
       >
         <Stack.Screen
@@ -27,6 +29,10 @@ export default function RootLayout() {
           }}
         />
       </Stack>
-    </>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: Colors.dark.background },
+})
