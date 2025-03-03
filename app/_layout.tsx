f26ac88111c2e26aa2eb0ae3c9f33f6c37f009ed
Dmitry Kovalev <trigger1610@gmail.com>
2025-03-03 16:41:58 +0400

    Add theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,46 +3,49 @@ import { SQLiteProvider } from 'expo-sqlite';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
-import { Colors } from '@/constants/Colors';
+import { customTheme } from './theme';
 
 export default function RootLayout() {
   return (
     <SQLiteProvider databaseName="stringLife">
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Stack
-          screenOptions={{
-            headerStyle: { backgroundColor: Colors.dark.background },
-            headerTintColor: Colors.dark.text,
-          }}
-        >
-          <Stack.Screen
-            name="index"
-            options={{
-              headerShown: false,
+      <PaperProvider theme={customTheme}>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Stack
+            screenOptions={{
+              headerStyle: { backgroundColor: customTheme.colors.background },
+              headerTintColor: customTheme.colors.text,
             }}
-          />
-          <Stack.Screen
-            name="add-instrument"
-            options={{
-              headerTransparent: true,
-              headerBackButtonDisplayMode: 'minimal',
-              headerTitle: '',
-            }}
-          />
-          <Stack.Screen
-            name="instrument"
-            options={{
-              headerShown: false,
-            }}
-          />
-        </Stack>
-      </View>
+          >
+            <Stack.Screen
+              name="index"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="add-instrument"
+              options={{
+                headerTransparent: true,
+                headerBackButtonDisplayMode: 'minimal',
+                headerTitle: '',
+              }}
+            />
+            <Stack.Screen
+              name="instrument"
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack>
+        </View>
+      </PaperProvider>
     </SQLiteProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: Colors.dark.background },
+  container: { flex: 1 },
 });
---
+++ app/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const customTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#DE7E00',
+    accent: '#9D7D5B',
+    background: '#151515',
+    scrim: '#E5DBD0',
+    placeholder: '#5F5D5A',
+  },
+};
