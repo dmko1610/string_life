71e2949bebbe72ed2b4a23145f6960e9afd61757
Dmitry Kovalev <trigger1610@gmail.com>
2025-03-04 18:19:16 +0400

    Fix prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['expo', 'eslint:recommended', 'prettier', 'plugin:react-hooks/recommended'],
+  extends: [
+    'expo',
+    'eslint:recommended',
+    'prettier',
+    'plugin:react-hooks/recommended',
+  ],
   plugins: ['prettier'],
   rules: { 'prettier/prettier': 'error' },
   env: { jest: true },
