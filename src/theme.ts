9306fceb1b97517d3c12af1bd910147b5c6bc50c
Dmitry Kovalev <trigger1610@gmail.com>
2025-06-17 19:45:53 +0400

    Add custom error colors

--- src/theme.ts
+++ src/theme.ts
@@ -7,13 +7,15 @@ export const lightTheme: MD3Theme = {
     primary: '#DE7E00',
     secondary: '#735943',
     tertiary: '#5a6238',
-    error: '#5a6238',
+    error: '#ba1a1a',
     primaryContainer: '#ffdcc1',
     onPrimaryContainer: '#6b3b03',
     secondaryContainer: '#ffdcc1',
     onSecondaryContainer: '#5a422d',
     tertiaryContainer: '#dfe8b2',
     onTertiaryContainer: '#434a23',
+    errorContainer: '#ffdad6',
+    onErrorContainer: '#93000a',
   },
 };
 
@@ -27,11 +29,15 @@ export const darkTheme: MD3Theme = {
     onSecondary: '#412c19',
     tertiary: '#c3cb98',
     onTertiary: '#2d330e',
+    error: '#ffb4ab',
+    onError: '#690005',
     primaryContainer: '#6b3b03',
     onPrimaryContainer: '#ffdcc1',
     secondaryContainer: '#5a422d',
     onSecondaryContainer: '#ffdcc1',
     tertiaryContainer: '#434a23',
-    onTertiaryContainer: '#dfe8b2'
+    onTertiaryContainer: '#dfe8b2',
+    errorContainer: '#93000a',
+    onErrorContainer: '#ffdad6',
   },
 };
